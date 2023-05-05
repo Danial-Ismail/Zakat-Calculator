c124be6f0258ed82dbd124f2dc79f98f2d014388
Danial-Ismail <121781837+Danial-Ismail@users.noreply.github.com>
2023-05-05 22:22:21 +0500

    add ternary operator

--- zakat calculator/app.js
+++ zakat calculator/app.js
@@ -3,7 +3,7 @@ function calculateZakat(){
     var silver=document.getElementById("silver").value;
     var cash=document.getElementById("cash").value;
 
-    var totalWealth=parseFloat(gold)+parseFloat(silver)+parseFloat(cash);
+    var totalWealth=(gold ?parseFloat(gold):0)+(silver?parseFloat(silver):0)+(cash?parseFloat(cash):0);
 
     var zakat=0.025*totalWealth;
 
